367d0089afcc5a1fb1e535fc521711fa181bffa1
houssem121 <hammami290@gmail.com>
2023-08-30 18:20:45 +0100

    angular

--- back-end/inscription/inscription/inscription/inscriptionspring/src/main/java/com/example/demo/inscriptionEntity/Role.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.inscriptionEntity;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public enum Role {
-    USER,
-    ADMIN
-}
--- back-end/inscription/inscription/inscriptionspring/src/main/java/com/example/demo/Repository/PaymentDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.productEN.PaymentDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentDetailsRepository extends JpaRepository<PaymentDetails, Long> {
-
-}
